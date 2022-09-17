a1b456b1f2d1f78defbf241143d50ceb00e49563
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-09-17 23:35:58 +0200

    Entire joke app with minimal webpack setup

--- 1.beginner-webpack/src/index.js
+++ 1.beginner-webpack/src/index.js
@@ -1,4 +1,11 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
+import angels from "./assets/Angels.jpg";
 
-console.log(generateJoke());
+const angelsImg = document.getElementById("angelsImg");
+angelsImg.src = angels;
+
+const jokeBtn = document.getElementById("jokeBtn");
+jokeBtn.addEventListener("click", generateJoke);
+
+generateJoke();
--- 1.beginner-webpack/webpack.config.js
+++ 1.beginner-webpack/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "development",
@@ -8,7 +10,20 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
+    filename: "[name][contenthash].js",
+    clean: true,
+    assetModuleFilename: "[name][ext]",
+  },
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -16,12 +31,28 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Webpack App",
       filename: "index.html",
+      template: "src/template.html",
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
