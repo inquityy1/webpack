d6eabaa7dae7ab2955075ae725f157035e0b52fc
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-09-19 21:47:03 +0200

    Add latest JS features with babel 7

--- 2.webpack/src/components/hello-world-button/hello-world-button.js
+++ 2.webpack/src/components/hello-world-button/hello-world-button.js
@@ -1,17 +1,19 @@
 import "./hello-world-button.scss";
 
 class HelloWorldButton {
+  buttonCssClass = "hello-world-button";
+
   render() {
     const button = document.createElement("button");
+    const body = document.querySelector("body");
     button.innerHTML = "Hello world";
-    button.classList.add("hello-world-button");
     button.onclick = function () {
       const p = document.createElement("p");
       p.innerHTML = "Hello world";
       p.classList.add("hello-world-text");
       body.appendChild(p);
     };
-    const body = document.querySelector("body");
+    button.classList.add(this.buttonCssClass);
     body.appendChild(button);
   }
 }
