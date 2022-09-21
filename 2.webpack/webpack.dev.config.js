7d653629a027922966dddd912e63df107f947cdc
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-09-21 15:01:01 +0200

    Setup for dev environment

--- 2.webpack/webpack.dev.config.js
+++ 2.webpack/webpack.dev.config.js
@@ -3,9 +3,12 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    "hello-world": "./src/hello-world.js",
+    angels: "./src/angels.js",
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js",
     path: path.resolve(__dirname, "./dist"),
     publicPath: "",
   },
@@ -47,9 +50,18 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      filename: "hello-world.html",
+      chunks: ["hello-world"],
       title: "Hello world",
-      template: "src/index.hbs",
       description: "Some description",
+      template: "src/page-template.hbs",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "angels.html",
+      chunks: ["angels"],
+      title: "Angels",
+      description: "Angels",
+      template: "src/page-template.hbs",
     }),
   ],
 };
