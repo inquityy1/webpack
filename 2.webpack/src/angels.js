a7945a9d6fe8063368ab079ae6607304e8fd62ad
inquityy1 <nebojsanikolic12350@yahoo.com>
2022-09-21 19:12:04 +0200

    Add multiple page app

--- 2.webpack/src/angels.js
+++ 2.webpack/src/angels.js
@@ -1,6 +1,5 @@
 import Heading from "./components/heading/heading";
 import AngelsImage from "./components/angels-image/angels-image";
-import React from "react";
 
 const heading = new Heading();
 heading.render("angels");
